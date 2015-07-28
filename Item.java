ea810573b9eb80a8b0df5a5371db2f04be48f93f
Kévin Ta <kevin.ta@edu.esiee.fr>
2015-07-28 18:49:36 +0200

    Exercice 7.20
    Création de la classe Item

---
+++ Item.java
@@ -0,0 +1,49 @@
+/**
+ * Classe item
+ */
+public class Item
+{
+    private String aDescription;
+    private double aWeight;
+
+    /**
+     * Constructeur de la classe item
+     */
+    public Item(final String pDescription, final double pWeight)
+    {
+        this.aDescription = pDescription;
+        this.aWeight = pWeight;
+    }
+    
+    /**
+     * Accesseur pour avoir la description de l'item
+     */
+    public String getDescription()
+    {
+        return this.aDescription;
+    }
+    
+    /**
+     * Accesseur pour avoir la description de l'item
+     */
+    public void setDescription(final String pDescription)
+    {
+        this.aDescription = pDescription;
+    }
+    
+    /**
+     * Accesseur pour avoir le poids de l'item
+     */
+    public double getWeight()
+    {
+        return this.aWeight;
+    }
+    
+    /**
+     * Mutateur pour définir le poids de l'item
+     */
+    public void setWeight(final double pWeight)
+    {
+        this.aWeight = pWeight;
+    }
+}
