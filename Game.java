d63b4b4ed426faa0d0b26802355b041df7fb72a6
kevin-ta <ta_kevin@hotmail.fr>
2017-05-29 23:44:26 +0200

    Exercice 7.53
    Création de la méthode main

--- Game.java
+++ Game.java
@@ -29,4 +29,9 @@ public class Game
         gui = new UserInterface(engine);
         engine.setGUI(gui);
     }
+    
+    public static void main(String[] args)
+    {
+        new Game();
+    }
 }
